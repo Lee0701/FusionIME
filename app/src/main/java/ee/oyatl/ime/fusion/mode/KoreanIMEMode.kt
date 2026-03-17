ec691d4c89499dc055658890d1e2e674701fc81d
Lee0701 <hgl1002@naver.com>
2026-03-17 15:40:27 +0900

    Add non-strict 391 layout

--- app/src/main/java/ee/oyatl/ime/fusion/mode/KoreanIMEMode.kt
+++ app/src/main/java/ee/oyatl/ime/fusion/mode/KoreanIMEMode.kt
@@ -187,7 +187,7 @@ abstract class KoreanIMEMode(
         converterType: ConverterType,
         listener: IMEMode.Listener
     ): Hangul3Set390391(listener) {
-        override val hangulCombiner: HangulCombiner = HangulCombiner(Hangul3Set.COMBINATION_390, correctOrders)
+        override val hangulCombiner: HangulCombiner = HangulCombiner(Hangul3Set.COMBINATION_390_391, correctOrders)
         override val hanjaConverter: HanjaConverter = converterType.create()
         override val textLayoutTable: LayoutTable = LayoutTable.from(LayoutExt.TABLE + LayoutQwerty.TABLE_QWERTY + Hangul3Set.TABLE_390)
         override val softKeyCodeMapper: SoftKeyCodeMapper get() = SoftKeyCodeMapper(mapOf(
@@ -203,9 +203,19 @@ abstract class KoreanIMEMode(
         converterType: ConverterType,
         listener: IMEMode.Listener
     ): Hangul3Set390391(listener) {
-        override val hangulCombiner: HangulCombiner = HangulCombiner(Hangul3Set.COMBINATION_391, correctOrders)
+        override val hangulCombiner: HangulCombiner = HangulCombiner(Hangul3Set.COMBINATION_390_391, correctOrders)
         override val hanjaConverter: HanjaConverter = converterType.create()
         override val textLayoutTable: LayoutTable = LayoutTable.from(LayoutExt.TABLE + LayoutQwerty.TABLE_QWERTY + Hangul3Set.TABLE_391)
+    }
+
+    class Hangul3Set391Strict(
+        correctOrders: Boolean,
+        converterType: ConverterType,
+        listener: IMEMode.Listener
+    ): Hangul3Set390391(listener) {
+        override val hangulCombiner: HangulCombiner = HangulCombiner(Hangul3Set.COMBINATION_391_STRICT, correctOrders)
+        override val hanjaConverter: HanjaConverter = converterType.create()
+        override val textLayoutTable: LayoutTable = LayoutTable.from(LayoutExt.TABLE + LayoutQwerty.TABLE_QWERTY + Hangul3Set.TABLE_391_STRICT)
     }
 
     class HangulOld2Set(
@@ -268,6 +278,7 @@ abstract class KoreanIMEMode(
                 Layout.Set2KS -> Hangul2SetKS(correctOrders, converterType, listener)
                 Layout.Set3390 -> Hangul3Set390(correctOrders, converterType, listener)
                 Layout.Set3391 -> Hangul3Set391(correctOrders, converterType, listener)
+                Layout.Set3391Strict -> Hangul3Set391Strict(correctOrders, converterType, listener)
                 Layout.Set2Old -> HangulOld2Set(correctOrders, converterType, listener)
                 Layout.Set3Old393 -> HangulOld3Set393(correctOrders, converterType, listener)
             }
@@ -283,6 +294,7 @@ abstract class KoreanIMEMode(
             return when(layout) {
                 Layout.Set2KS -> "한2"
                 Layout.Set3390, Layout.Set3391 -> "한3"
+                Layout.Set3391Strict -> "한3s"
                 Layout.Set2Old -> "ᄒᆞ2"
                 Layout.Set3Old393 -> "ᄒᆞ3"
             }
@@ -308,6 +320,7 @@ abstract class KoreanIMEMode(
         Set2KS(R.string.korean_layout_hangul_2set_ks),
         Set3390(R.string.korean_layout_hangul_3set_390),
         Set3391(R.string.korean_layout_hangul_3set_391),
+        Set3391Strict(R.string.korean_layout_hangul_3set_391),
         Set2Old(R.string.korean_layout_old_hangul_2set_ks),
         Set3Old393(R.string.korean_layout_old_hangul_3set_393)
     }
