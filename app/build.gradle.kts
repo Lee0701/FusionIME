5dd2616412f4ae65599464b44bf4390ef1bf8eb6
Automated Publisher <actions@users.noreply.github.com>
2026-06-29 12:34:26 +0900

    Bump version Mon Jun 29 12:34:26 KST 2026 6fe98188226286f2d3722b5fadd685feddf5945d

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ configure<ApplicationExtension> {
         applicationId = "ee.oyatl.ime.fusion"
         minSdk = 21
         targetSdk = 36
-        versionCode = 15
-        versionName = "20260502-15-4b845c1"
+        versionCode = 16
+        versionName = "20260629-16-6fe9818"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
