0a54fe3102fdc99785c4ffe770e2770037e4edf2
kompackvenom <kompackvenom@users.noreply.github.com>
2026-08-17 18:45:00 +0900

    Adapt long-press fading to dark mode

--- app/keyboard/src/main/java/ee/oyatl/ime/keyboard/PaleViewFilter.kt
+++ app/keyboard/src/main/java/ee/oyatl/ime/keyboard/PaleViewFilter.kt
@@ -1,5 +1,6 @@
 package ee.oyatl.ime.keyboard
 
+import android.content.res.Configuration
 import android.graphics.ColorMatrixColorFilter
 import android.graphics.Paint
 import android.view.View
@@ -7,19 +8,14 @@ import android.view.View
 class PaleViewFilter(
     originalColorWeight: Float = 0.30f
 ) {
-    private val paint = Paint().apply {
-        val whiteOffset = 255f * (1f - originalColorWeight)
-        colorFilter = ColorMatrixColorFilter(floatArrayOf(
-            originalColorWeight, 0f, 0f, 0f, whiteOffset,
-            0f, originalColorWeight, 0f, 0f, whiteOffset,
-            0f, 0f, originalColorWeight, 0f, whiteOffset,
-            0f, 0f, 0f, 1f, 0f
-        ))
-    }
+    private val lightPaint = createPaint(originalColorWeight, 255f)
+    private val darkPaint = createPaint(originalColorWeight, 0f)
     private var target: View? = null
     private var originalLayerType: Int? = null
 
     fun apply(view: View) {
+        val nightMode = view.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        val paint = if(nightMode == Configuration.UI_MODE_NIGHT_YES) darkPaint else lightPaint
         if(target !== view) {
             clear()
             target = view
@@ -38,5 +34,17 @@ class PaleViewFilter(
         }
         target = null
         originalLayerType = null
+    }
+
+    private fun createPaint(originalColorWeight: Float, blendColor: Float): Paint {
+        val colorOffset = blendColor * (1f - originalColorWeight)
+        return Paint().apply {
+            colorFilter = ColorMatrixColorFilter(floatArrayOf(
+                originalColorWeight, 0f, 0f, 0f, colorOffset,
+                0f, originalColorWeight, 0f, 0f, colorOffset,
+                0f, 0f, originalColorWeight, 0f, colorOffset,
+                0f, 0f, 0f, 1f, 0f
+            ))
+        }
     }
 }
